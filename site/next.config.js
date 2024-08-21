5cb4479ec21793e5e3348542dafc49778dad6b7c
Hayden <cat@reo.moe>
2024-08-21 13:21:36 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'reo\'s registry',
+    description: 'reo\'s custom kasm registry',
+    icon: 'https://reoccurcat.github.io/kasm-registry/1.0/img/logo.svg',
+    listUrl: 'https://reoccurcat.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/reoccurcat/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
